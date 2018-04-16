4d8c836f3f6b96402a450e6b2d7a05befef165bd
Sk Saddam Hosan <sksddmhosan@gmail.com>
2018-04-16 17:52:00 +0530

    add new file stock and english

---
+++ DocumentEbook/EnglishDictionaryDocument.java
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+1. evaluator : someone whose job is to judge the quality, importance, amount, or value of something
+2. amenity : a desirable or useful feature or facility of a building or place.
+3. gamble : take risky action in the hope of a desired result.
+4. introspection : the examination or observation of one's own mental and emotional processes.
