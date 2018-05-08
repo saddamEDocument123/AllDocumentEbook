9d6b85c1d2022845690fdd4a75541987b9e88363
Sk Saddam Hosan <sksddmhosan@gmail.com>
2018-05-08 11:57:03 +0530

    add some context

---
+++ DocumentEbook/Angularjs2-Doc/Angularjs2PracticalDocument.java
@@ -0,0 +1,21 @@
+/****
+
+
+
+ */
+
+ 1. How to create new project in angularJs 2?
+ =>
+    Step 1. Set up the Development Environment 
+        npm install -g @angular/cli
+
+    Step 2. Create a new project 
+        ng new my-app
+
+    Step 3: Serve the application 
+        ng serve
+
+
+2. 
+
+    
