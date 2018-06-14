8f5ccc7be3d8693db07af9a0580278f0c2b6209e
Sk Saddam Hosan <sksddmhosan@gmail.com>
2018-06-14 17:50:35 +0530

    add new stock market book

--- DocumentEbook/Stock-Doc/StockAndTradeDocument.java
+++ DocumentEbook/Stock-Doc/StockAndTradeDocument.java
@@ -1,6 +1,13 @@
 
 
 
+KYC verification for PAN ;
+
+  1. 2 Passport pic
+  2. PAN card
+  3. Address proof , voter card or aadhar card
+  4. Bank details
+
 
 1. Research the companies that are listed on NSE and BSE , and lookout for IPOs
 2. others are the wide range of discount brokers, Zerodha, Sharekhan, Motilal Oswal , Angelbroking to name a few.
