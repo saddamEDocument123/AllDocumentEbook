67dad3e0f21443013dd032c0d0d4aa331524cba1
Sk Saddam Hosan <sksddmhosan@gmail.com>
2018-06-21 18:25:16 +0530

    spring boot setup done with database ..

--- DocumentEbook/SpringBoot/SpringBootDocument.java
+++ DocumentEbook/SpringBoot/SpringBootDocument.java
@@ -7,47 +7,68 @@
     2. add some pom dependecy
 
 
-      <properties>
-         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-         <java.version>1.8</java.version>
-        </properties>
+      <
+	<parent>
+		<groupId>org.springframework.boot</groupId>
+		<artifactId>spring-boot-starter-parent</artifactId>
+		<version>1.5.14.RELEASE</version>
+		<relativePath/> <!-- lookup parent from repository -->
+	</parent>
 
+	<properties>
+		<project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+		<project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
+		<java.version>1.8</java.version>
+	</properties>
 
-     <parent>
-        <groupId>org.springframework.boot</groupId>
-        <artifactId>spring-boot-starter-parent</artifactId>
-         <version>2.0.2.RELEASE</version>
-    </parent>
-
-
-        <dependencies>
-        <dependency>
-        <groupId>junit</groupId>
-        <artifactId>junit</artifactId>
-        <scope>test</scope>
-        </dependency>
-        
-        <dependency>
-        <groupId>org.springframework.boot</groupId>
-        <artifactId>spring-boot-starter-web</artifactId>
-        </dependency>
-        
-    </dependencies>
-
-    // for create war file or jar file u have to add maven plugin like this --
-
-     <build>
-  
-     <plugins>
-			<!-- Update to latest version of JAVA -->
-			<plugin>
+	<dependencies>
+		<dependency>
 			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-data-jpa</artifactId>
+		</dependency>
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-web</artifactId>
+		</dependency>
+
+        
+		<dependency> // this is also optional
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-devtools</artifactId>
+			<scope>runtime</scope>
+		</dependency>
+
+
+		<dependency>
+			<groupId>mysql</groupId>
+			<artifactId>mysql-connector-java</artifactId>
+			<scope>runtime</scope>
+		</dependency>
+
+        
+		<dependency> // this is optional
+			<groupId>org.projectlombok</groupId>
+			<artifactId>lombok</artifactId>
+			<optional>true</optional>
+		</dependency>
+
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-test</artifactId>
+			<scope>test</scope>
+		</dependency>
+	</dependencies>
+
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>org.springframework.boot</groupId>
 				<artifactId>spring-boot-maven-plugin</artifactId>
 			</plugin>
-
 		</plugins>
-        <finalName>SpringBootTest</finalName>
-     </build>
+	</build>
+
+
 
     3. create App.java class u can change this file name
         
@@ -89,6 +110,102 @@
             }
 
         }
+
+    6. Add database like this  :
+
+    create application.properties file -
+
+    jdbc.driverClassName = com.mysql.jdbc.Driver
+    jdbc.url = jdbc:mysql://localhost:3306/springboot
+    jdbc.username = root
+    jdbc.password = root
+    hibernate.dialect = org.hibernate.dialect.MySQLDialect
+
+    server.port=8001
+
+
+    7. then create database loaded file like this :
+
+       DBConfig.java   --- 
+
+
+        package com.spring.boot.orm.api.config;
+
+        import java.util.Properties;
+
+        import javax.sql.DataSource;
+
+        import org.hibernate.SessionFactory;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.context.annotation.PropertySource;
+        import org.springframework.jdbc.datasource.DriverManagerDataSource;
+        import org.springframework.orm.hibernate5.HibernateTransactionManager;
+        import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+
+
+        @PropertySource(value= {"classpath:application.properties"})
+        @Configuration
+        public class DBConfig {
+
+            @Value("${jdbc.driverClassName}")
+            private String driverClass;
+            @Value("${jdbc.url}")
+            private String url;
+            @Value("${jdbc.username}")
+            private String username;
+            @Value("${jdbc.password}")
+            private String password;
+            @Value("${hibernate.dialect}")
+            private String dialect;
+            
+            
+            @Bean
+            public DataSource getDataSource() {
+                
+                DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
+                dataSource.setDriverClassName(driverClass);
+                return dataSource;
+                
+            }
+            
+            @Bean
+            public LocalSessionFactoryBean sessionFactory() {
+                
+                LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+                factory.setDataSource(getDataSource());
+                factory.setHibernateProperties(hibernateProperties());
+                factory.setPackagesToScan(new String[] {"com.spring.boot.orm.api"});
+                return factory;
+                
+                
+            }
+            
+            private Properties hibernateProperties() {
+                Properties properties = new Properties();
+                properties.put("hibernate.dialect", dialect);
+                properties.put("hibernate.hbm2ddl.auto", "update");
+                properties.put("hibernate.show_sql", "true");
+                properties.put("hibernte.format_sql", "true");
+                return properties;
+            }
+            
+            @Bean
+            public HibernateTransactionManager transactionManager(SessionFactory factory) {
+                HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+                transactionManager.setSessionFactory(factory);
+                return transactionManager;
+                
+            }
+            
+            
+        }
+
+
+
+
 
     6. how to run the application jar file 
 
